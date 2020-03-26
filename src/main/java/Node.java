78a153f90e433ebb96a01d69b9659fb8635ab0b4
elonskolnik <42852166+elonskolnik@users.noreply.github.com>
2020-03-26 19:58:49 -0400

    added definition for constructor, all tests passing

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -8,19 +8,39 @@ public class Node {
     public Map<Integer, Node> nextNodes;
 
     //root node constructor
-    public Node(int id, String storyContent) throws IllegalArgumentException{
+    //add character limit here?
+    public Node(int idIn, String storyContentIn) throws IllegalArgumentException{
         //should throw invalid argument exceptions higher up:
         //id must be unique, each story must have exactly one root node
 
-
+        if (idIn < 1){
+            throw new IllegalArgumentException("ID must be a positive number");
+        }
+        id = idIn;
+        storyContent = storyContentIn;
     }
 
     //child node constructor
-    public Node(int id, String storyContent, Node parentNode, int choiceValue) throws IllegalArgumentException{
+    public Node(int idIn, String storyContentIn, Node parentNodeIn, int choiceValueIn) throws IllegalArgumentException{
         //should throw invalid argument exceptions higher up:
         //id must be unique choiceValue must be unique to parent
 
+        if (idIn < 1){
+            throw new IllegalArgumentException("ID must be a positive number");
+        }
 
+        if(parentNodeIn == null){
+            throw new IllegalArgumentException("Parent node does not exist");
+        }
+
+        if(choiceValueIn < 1){
+            throw new IllegalArgumentException("Choice value must be a positive number");
+        }
+
+        id = idIn;
+        storyContent = storyContentIn;
+        parentNode = parentNodeIn;
+        choiceValue = choiceValueIn;
     }
 
     public String getStoryContent(){
