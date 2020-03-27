18c4fa72c8cfb93a93531c52d190b2ffccb62ebf
leianna <lchen4@ithaca.edu>
2020-03-27 11:03:08 -0400

    Story

--- src/main/java/Story.java
+++ src/main/java/Story.java
@@ -4,16 +4,15 @@ import java.util.Map;
 public class Story extends Node {
     public String Tittle;
     LinkedList<String> Tags = new LinkedList<String>();
-    Map<Integer,Node> stroyNode = new Map<,Node>();
     public Node root;
     public Node currentNode;
 
-    public Story(int idIn,String titleIn, String storyContentIn, LinkedList tagsIn, Map stroyNode) throws IllegalArgumentException {
+    public Story(int idIn,String titleIn, String storyContentIn, LinkedList tagsIn) throws IllegalArgumentException {
         super(idIn, storyContentIn);
         Tittle = titleIn;
         Tags = tagsIn;
-
-
+        Map<Integer,Node> stroyNode = new Map<,Node>();
+        
     }
 
 }
