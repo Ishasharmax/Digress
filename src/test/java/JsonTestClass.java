a83d49e2d562b5e7668315a03dc04aa434bcbf07
Mike Santo <aero10599@gmail.com>
2020-04-16 18:18:16 -0400

    JUnit stuff added to test file

--- src/test/java/JsonTestClass.java
+++ src/test/java/JsonTestClass.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.lang.String;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class JsonTestClass {
+
+
+    @Test
+    void story1Tests(){
+
+    }
+
+    @Test
+    void story2Tests(){
+
+    }
+
+    @Test
+    void story3Tests(){
+
+    }
 }
