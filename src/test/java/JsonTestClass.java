09fea1ac2318146ff3d55ed729b63484e0f1f9d7
Mike Santo <aero10599@gmail.com>
2020-04-16 23:38:53 -0400

    tests mostly written, just waiting on implementation info necessary to continue

--- src/test/java/JsonTestClass.java
+++ src/test/java/JsonTestClass.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.lang.String;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,7 +11,27 @@ public class JsonTestClass {
 
 
     @Test
-    void story1Tests(){
+    void story1Tests() throws IOException {
+
+        //importing a Story object
+        Story story1 = JsonUtil.fromJsonFile("src/test/Story1.json", Story.class);
+        //checking ID
+        assertEquals(5, story1.getID());
+        //checking title
+        assertEquals("Story", story1.getTitle());
+        //checking root content
+        assertEquals("Content", story1.getRootContent());
+        //checking tags
+        assertEquals("Adventure", story1.getTags().getFirst());
+
+        //importing node
+        Node node1 = JsonUtil.fromJsonFile("src/test/s1n1.json", Node.class);
+        //checking Node getters
+        assertEquals(2, node1.getId());
+        assertEquals("Content for first node in S1", node1.getStoryContent());
+
+        //adding node to story
+        story1.addNode(node1);
 
     }
 
