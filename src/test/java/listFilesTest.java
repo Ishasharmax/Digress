e55944699085bae18a82953684e31884fc3f9f9f
jmendezdietsch <jmendezdietsch@ithaca.edu>
2020-04-22 12:35:11 -0400

    The folder version of listAllFiles makes a directory and checks if it already exists, with a test text file

--- src/test/java/listFilesTest.java
+++ src/test/java/listFilesTest.java
@@ -1,15 +1,27 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 
 
 public class listFilesTest{
     @Test // = Passed but needs files to be existing in C: folder
-    public void listFilesTest() throws NullPointerException {
-        File folder = new File("C:\\Test");
-        //folder.mkdir();
+    public void listFilesTest() throws NullPointerException, IOException {
+        String basicPath = "C:\\ListFilesTest";
+        File folder = new File(basicPath);
+        if(folder.mkdir()){
+            System.out.println("New Directory made");
+        }
+        else{
+            System.out.println("Directory exists");
+        }
+        File txtFile = new File(basicPath + "\\testFile1.txt");
+        FileWriter writer = new FileWriter(txtFile);
+        writer.write("Test data");
+        writer.close();
+
         listFiles listFiles = new listFiles();
         System.out.println("reading files");
         listFiles.listAllFiles(folder);
