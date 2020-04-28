ba1c5da0be31e5f3b8368c70058ee15c887812b0
Mike Santo <aero10599@gmail.com>
2020-04-28 16:01:49 -0400

    changed Story class

--- src/main/java/Story.java
+++ src/main/java/Story.java
@@ -1,5 +1,4 @@
-import testFilePackage.GlobalVariables;
-
+import java.io.*;
 import java.util.*;
 
 public class Story {
@@ -7,12 +6,11 @@ public class Story {
     private int id;
     private String title;
     private String rootCont;
-    private int choiceVal;
     private Node root;
     private Node currentNode;
     private HashMap<Integer, Node> storyNodes;
+    private HashMap<Integer, ArrayList<Integer>> nodeConnections;
     LinkedList<String> tags;
-    GlobalVariables variables;
 
     //used by Json
     public Story(){
@@ -45,33 +43,24 @@ public class Story {
         root = new Node(1, rootCont);
         currentNode = root;
         storyNodes = new HashMap();
+        nodeConnections = new HashMap<>();
         storyNodes.put(1, root);
-        variables = new GlobalVariables();
-        tags = new LinkedList<String>();
-    }
-
-    public void addExistingNode(Node nodeIn) throws IllegalArgumentException{
-        if (storyNodes == null){
-            this.storyNodes = new HashMap<>();
-            this.root = new Node(1, rootCont);
-            storyNodes.put(1, root);
-        }
-        if(nodeIn == null){
-            throw new IllegalArgumentException("Node cannot be null");
-        }
-        //already exists
-        if(findNode(nodeIn.getId()) != null){
-            throw new IllegalArgumentException("A node already exists with that ID");
-        }
-        storyNodes.put(nodeIn.getId(), nodeIn);
-        currentNode = nodeIn;
+        nodeConnections.put(root.getId(), new ArrayList<>());
+        tags = new LinkedList<>();
     }
 
     public Node getNext(int choiceValue){
-        if(storyNodes.size()-1 < choiceValue) {
-            throw new IllegalArgumentException("A node with this choice value does not exist");
+        if (nodeConnections.get(currentNode.getId()).isEmpty()){
+            throw new IllegalArgumentException("Node has no children");
         }
-        currentNode = currentNode.getNext(choiceValue);
+        if (choiceValue > nodeConnections.get(currentNode.getId()).size()){
+            throw new IllegalArgumentException("Choice value doesn't exist");
+        }
+        if (choiceValue < 1){
+            throw new IllegalArgumentException("Choice value must be a positive number greater than 0");
+        }
+        int ID = nodeConnections.get(currentNode.getId()).get(choiceValue-1);
+        currentNode = findNode(ID);
         return currentNode;
     }
 
@@ -92,6 +81,7 @@ public class Story {
         else{
             throw new IllegalArgumentException("Invalid choice");
         }
+
     }
 
     public void editNodeStoryContent(int nodeID, String newStoryContent){
@@ -105,25 +95,25 @@ public class Story {
         nodeToChange.editStoryContent(newStoryContent);
     }
 
-    public void editNodeChildren(int nodeID, int child1ChoiceValue, int child2ChoiceValue){
-        Node nodeToChange = findNode(nodeID);
-        if (nodeToChange.getNextNodes().isEmpty()){
-            throw new IllegalArgumentException("This node has no children");
-        }
-        HashMap<Integer, String> nextConditions = nodeToChange.getNextConditions();
-        HashMap<Integer, Node> nextNodes = nodeToChange.getNextNodes();
-        Node child1 = nodeToChange.getNext(child1ChoiceValue);
-        Node child2 = nodeToChange.getNext(child2ChoiceValue);
-        if (child1 == null || child2 == null){
-            throw new IllegalArgumentException("A node with this choice value does not exist");
-        }
-        String child1Condition = nextConditions.get(child1ChoiceValue);
-        String child2Condition = nextConditions.get(child2ChoiceValue);
-        nextConditions.replace(child1ChoiceValue, child2Condition);
-        nextConditions.replace(child2ChoiceValue, child1Condition);
-        nextNodes.replace(child1ChoiceValue, child2);
-        nextNodes.replace(child2ChoiceValue, child1);
-    }
+//    public void editNodeChildren(int nodeID, int child1ChoiceValue, int child2ChoiceValue){
+//        Node nodeToChange = findNode(nodeID);
+//        if (nodeToChange.getNextNodes().isEmpty()){
+//            throw new IllegalArgumentException("This node has no children");
+//        }
+//        HashMap<Integer, String> nextConditions = nodeToChange.getNextConditions();
+//        HashMap<Integer, Node> nextNodes = nodeToChange.getNextNodes();
+//        Node child1 = nodeToChange.getNext(child1ChoiceValue);
+//        Node child2 = nodeToChange.getNext(child2ChoiceValue);
+//        if (child1 == null || child2 == null){
+//            throw new IllegalArgumentException("A node with this choice value does not exist");
+//        }
+//        String child1Condition = nextConditions.get(child1ChoiceValue);
+//        String child2Condition = nextConditions.get(child2ChoiceValue);
+//        nextConditions.replace(child1ChoiceValue, child2Condition);
+//        nextConditions.replace(child2ChoiceValue, child1Condition);
+//        nextNodes.replace(child1ChoiceValue, child2);
+//        nextNodes.replace(child2ChoiceValue, child1);
+//    }
 
     public void addNode(String storyContent, int parentID, int choiceValue, String condition){
         if (findNode(parentID) == null) {
@@ -132,19 +122,49 @@ public class Story {
         if (storyContent.equals("") || storyContent == " "){
             throw new IllegalArgumentException("Story content cannot be empty");
         }
-        choiceVal = choiceValue;
-        Node parent = findNode(parentID);
         int nodeID = storyNodes.size() + 1;
-        Node sNode = new Node(nodeID, storyContent, parent);
+        while (storyNodes.get(nodeID) != null){
+            nodeID += 1;
+        }
+        Node sNode = new Node(nodeID, storyContent);
         storyNodes.put(nodeID, sNode);
         currentNode = storyNodes.get(nodeID); //Every time a node gets added, it becomes the current node
-        parent.setChild(choiceValue, condition, sNode);
     }
+
+    public void linkNodes(int parentID, int childID){
+        if (parentID == childID){
+            throw new IllegalArgumentException("Cannot set Node as it's own child");
+        }
+        if (findNode(parentID) == null) {
+            throw new IllegalArgumentException("Parent does not exist");
+        }
+        if (findNode(childID) == null) {
+            throw new IllegalArgumentException("Child does not exist");
+        }
+        if (nodeConnections.get(parentID).contains(childID)){
+            throw new IllegalArgumentException("This node is already a child");
+        }
+        nodeConnections.get(parentID).add(childID);
+    }
+
+    public void removeReferences(int nodeID){
+        Iterator<Map.Entry<Integer, ArrayList<Integer>>> itr = nodeConnections.entrySet().iterator();
+        while (itr.hasNext()){
+            Map.Entry<Integer, ArrayList<Integer>> entry = itr.next();
+            if (entry.getValue().contains(nodeID)){
+                entry.getValue().remove(nodeID);
+            }
+        }
+    }
+
     public void deleteNode(int nodeID) throws IllegalArgumentException{
         if (findNode(nodeID)==null){
             throw new IllegalArgumentException("Node is not exist");
         }else{
             storyNodes.remove(nodeID);
+            removeReferences(nodeID);
+            //TODO handling children after deletion
+
         }
     }
     Node findNode(int nodeID) throws IllegalArgumentException{ //hardcoded test to supplement addnode
@@ -167,16 +187,6 @@ public class Story {
         return node;
     }
 
-    public String printNode(int nodeId){
-        Node node = findNode(nodeId);
-        String nodeStr = node.getStoryContent();
-        Map nextConditions = node.getNextConditions();
-        for (int i = 1; i <= nextConditions.size(); i++){
-            nodeStr += "\n(" + i + ") " + nextConditions.get(i);
-        }
-        return nodeStr;
-    }
-
     public String printAllNodes(){
         String allNodes = "";
         for(int i = 1; i <= storyNodes.size(); i++){
@@ -188,40 +198,8 @@ public class Story {
         return allNodes;
     }
 
-    public void addVariable(String name, String type, Object value){
-        if (!type.equals("string") && !type.equals("int")){
-            throw new IllegalArgumentException("Must be a valid type");
-        }
-        if (type.equals("string")){
-            variables.addString(name, value.toString());
-        }
-        if (type.equals("int")){
-            variables.addInt(name, (Integer) value);
-        }
-    }
-
     public void addTag(String tagToAdd){
         tags.add(tagToAdd);
-    }
-
-    public Object getVariable(String name){
-        return variables.getVariable(name);
-    }
-
-    public void removeVariable(String name){
-        variables.removeVariable(name);
-    }
-
-    public void clearVariables(){
-        variables.clearVariables();
-    }
-
-    public void printVariable(String name){
-        variables.printVariable(name);
-    }
-
-    public void editVariable(String name, Object newValue){
-        variables.editVariable(name, newValue);
     }
 
     public Node getRoot(){
@@ -252,17 +230,8 @@ public class Story {
         return rootCont;
     }
 
-
-    public int getChoiceVal(){
-        return choiceVal;
-    }
-
     public LinkedList<String> getTags(){
         return tags;
-    }
-
-    public GlobalVariables getGlobalVariables(){
-        return variables;
     }
 
 }
