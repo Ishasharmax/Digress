8f3b9efd06b78af135206ec131d776cf45bb911f
jmendezdietsch <jmendezdietsch@ithaca.edu>
2020-04-29 19:56:44 -0400

    Two more tests added for deleting a node without children and deleting the root

--- src/test/java/newDeleteTests.java
+++ src/test/java/newDeleteTests.java
@@ -31,10 +31,29 @@ public class newDeleteTests {
         testStory2.addNode("I'm a child of 6, but i should be relinked to 2", 1, 5, "Fifth choice");
         testStory2.addNode("I'm a child of 6, but i should be relinked to 3", 1, 6, "Sixth choice");
         //Testing if size is correctly 7
-        assertEquals(testStory1.getStoryNodes().size(),7);
+        assertEquals(testStory2.getStoryNodes().size(),7);
         //Find the necessary node to delete
-        testStory1.findNode(5);
+        testStory2.findNode(5);
         //Deleting Node and should delete children
-        testStory1.deleteNode(5);
+        testStory2.deleteNode(5);
+
+        //Based on node without children
+        Story testStory3 = new Story(1,"Story", "This is test content for the root");
+        testStory3.addNode("Test Content 3, but delete me", 1, 1, "First choice");
+        //Testing if size is correctly 2
+        assertEquals(testStory3.getStoryNodes().size(),2);
+        //Find the necessary node to delete
+        testStory3.findNode(2);
+        //Deleting Node without children
+        testStory3.deleteNode(2);
+
+        //Based on root
+        Story testStory4 = new Story(1,"Story", "This is test content for the root");
+        //Testing if size is correctly 1
+        assertEquals(testStory4.getStoryNodes().size(),1);
+        //Find the necessary node to delete
+        testStory4.findNode(1);
+        //Deleting Node without children
+        testStory4.deleteNode(1);
     }
 }
