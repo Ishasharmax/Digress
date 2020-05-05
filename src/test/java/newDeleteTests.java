0c4e8e9380907b7b527fa21aca010da34a20235c
jmendezdietsch <jmendezdietsch@ithaca.edu>
2020-05-05 14:50:17 -0400

    Fixed errors in deleteTests

--- src/test/java/newDeleteTests.java
+++ src/test/java/newDeleteTests.java
@@ -8,12 +8,12 @@ public class newDeleteTests {
     void newDeleteNodeTests(){
         //Based on deleting nodes
         Story testStory1 = new Story(1,"Story", "This is test content for the root");
-        testStory1.addNode("Test Content 1", 1, 1, "First choice");
-        testStory1.addNode("Test Content for another Node", 1, 2, "Second choice");
-        testStory1.addNode("Woah more test content", 1, 3, "Third choice");
-        testStory1.addNode("Delete Me", 1, 4, "Fourth choice");
-        testStory1.addNode("I'm a child of 6, should be deleted", 1, 5, "Fifth choice");
-        testStory1.addNode("I'm a child of 6, should also be deleted", 1, 6, "Sixth choice");
+        testStory1.addNode("Test Content 1");
+        testStory1.addNode("Test Content for another Node");
+        testStory1.addNode("Woah more test content");
+        testStory1.addNode("Delete Me");
+        testStory1.addNode("I'm a child of 6, should be deleted");
+        testStory1.addNode("I'm a child of 6, should also be deleted");
         //Testing if size is correctly 7
         assertEquals(testStory1.getStoryNodes().size(),7);
         //Find the necessary node to delete
@@ -24,12 +24,12 @@ public class newDeleteTests {
 
         //Based on relinking nodes
         Story testStory2 = new Story(1,"Story", "This is test content for the root");
-        testStory2.addNode("Test Content 1", 1, 1, "First choice");
-        testStory2.addNode("Test Content for another Node", 1, 2, "Second choice");
-        testStory2.addNode("Woah more test content", 1, 3, "Third choice");
-        testStory2.addNode("Delete Me", 1, 4, "Fourth choice");
-        testStory2.addNode("I'm a child of 6, but i should be relinked to 2", 1, 5, "Fifth choice");
-        testStory2.addNode("I'm a child of 6, but i should be relinked to 3", 1, 6, "Sixth choice");
+        testStory2.addNode("Test Content 1");
+        testStory2.addNode("Test Content for another Node");
+        testStory2.addNode("Woah more test content");
+        testStory2.addNode("Delete Me");
+        testStory2.addNode("I'm a child of 6, but i should be relinked to 2");
+        testStory2.addNode("I'm a child of 6, but i should be relinked to 3");
         //Testing if size is correctly 7
         assertEquals(testStory2.getStoryNodes().size(),7);
         //Find the necessary node to delete
@@ -39,7 +39,7 @@ public class newDeleteTests {
 
         //Based on node without children
         Story testStory3 = new Story(1,"Story", "This is test content for the root");
-        testStory3.addNode("Test Content 3, but delete me", 1, 1, "First choice");
+        testStory3.addNode("Test Content 3, but delete me");
         //Testing if size is correctly 2
         assertEquals(testStory3.getStoryNodes().size(),2);
         //Find the necessary node to delete
@@ -58,16 +58,16 @@ public class newDeleteTests {
 
         //Deleting multiple nodes
         Story testStory5 = new Story(1,"Story", "This is test content for the root");
-        testStory5.addNode("Test Content 1", 1, 1, "First choice");
-        testStory5.addNode("Test Content for another Node", 1, 2, "Second choice");
-        testStory5.addNode("Woah more test content", 1, 3, "Third choice");
-        testStory5.addNode("Delete Me", 1, 4, "Fourth choice");
-        testStory5.addNode("I'm a child of 6, but i should be relinked to 2", 1, 5, "Fifth choice");
-        testStory5.addNode("I'm a child of 6, but i should be relinked to 3", 1, 6, "Sixth choice");
-        testStory5.addNode("I'm a new node with more content", 1, 7, "Seventh choice");
-        testStory5.addNode("I should be deleted though", 1, 8, "Eighth choice");
-        testStory5.addNode("I'm a child of 7", 1, 9, "Ninth choice");
-        testStory5.addNode("I'm a child of 8, relinked with 7", 1, 10, "Tenth choice");
+        testStory5.addNode("Test Content 1");
+        testStory5.addNode("Test Content for another Node");
+        testStory5.addNode("Woah more test content");
+        testStory5.addNode("Delete Me");
+        testStory5.addNode("I'm a child of 6, but i should be relinked to 2");
+        testStory5.addNode("I'm a child of 6, but i should be relinked to 3");
+        testStory5.addNode("I'm a new node with more content");
+        testStory5.addNode("I should be deleted though");
+        testStory5.addNode("I'm a child of 7");
+        testStory5.addNode("I'm a child of 8, relinked with 7");
         //Testing if size is correctly 10
         assertEquals(testStory5.getStoryNodes().size(),10);
         //Find the necessary node to delete
